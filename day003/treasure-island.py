bf3e7d7f94b43eae1dd4b71479d059df925bf638
geogebra04 <geogebra04@gmail.com>
2021-01-05 22:34:15 +0100

    day003 2nd commit

--- day003/treasure-island.py
+++ day003/treasure-island.py
@@ -22,7 +22,16 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-if input("There is two ways, choose one : 'right' or 'left'") == "right":
-    if input("A lake : 'swim' or 'wait' a boat") == "wait":
-        if input("
-
+ans1 = input("There is two ways, choose one : 'right' side or 'left' side > ")
+if (ans1 == "right"):
+    ans2 = input("Woot, A beautiful river in the font : 'swim' or 'wait' a boat to cross the river > ")
+    if  ans2 == "swim":
+        ans3 = input("A high high high Waterfall : 'jump' or 'back' > ")
+        if ans3  == "jump":
+            print("you find the treasure...")
+        else:
+            print("you failed, there is no way to go back\n GAME OVER")
+    else:
+        print("wait wait wait, still waiting... RIP\n GAME OVER")
+else:
+    print("you fall in a black hole\nGAME OVER")
